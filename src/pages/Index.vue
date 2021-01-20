59583a3729d140de3102405ff5fd37b7669878dc
ivancr2 <ivancalixto1@gmail.com>
2021-01-20 16:00:18 -0300

    alteração nos nomes das variáveis

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -21,7 +21,7 @@
       <q-btn color="primary" v-on:click="predict">Reconhecer imagem</q-btn>
     </div>
     <div>
-      <div class="element">{{ predictedValue }}</div>
+      <div class="element">{{ msgPredicao }}</div>
     </div>
   </q-page>
 </template>
@@ -34,8 +34,8 @@ export default {
   name: "PageIndex",
   data() {
     return {
-      predictedValue: "Clique em Reconhecer imagem",
-      valueToPredict: "",
+      msgPredicao: "Clique em Reconhecer imagem",
+      valorPredito: "",
       model: tf.sequential(),
       file: null,
       img: truck,
@@ -74,18 +74,17 @@ export default {
     carregaImagem() {
       this.img = URL.createObjectURL(this.file);
       console.log(this.file);
-      this.predictedValue = "Clique em reconhecer imagem";
+      this.msgPredicao = "Clique em Reconhecer imagem";
     },
     predict() {
-      this.valueToPredict = document.getElementById("imagem");
-      let arrInput = tf.browser.fromPixels(this.valueToPredict); //
-      this.valueToPredict = tf.image
+      this.valorPredito = document.getElementById("imagem");
+      let arrInput = tf.browser.fromPixels(this.valorPredito); //
+      this.valorPredito  = tf.image
         .resizeBilinear(arrInput, [32, 32])
         .reshape([1, 32, 32, 3]);
 
-      let valor = this.model.predict(this.valueToPredict);
-
-      this.predictedValue = this.labels[valor.argMax(-1).dataSync()[0]];
+      let valor = this.model.predict(this.valorPredito);
+      this.msgPredicao = this.labels[valor.argMax(-1).dataSync()[0]];
     }
   }
 };
